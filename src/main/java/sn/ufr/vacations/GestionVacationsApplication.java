481e0c5d9a8bb8e91035ebcf3842627089420ae0
morkebe <morkhoudiakebe@gmail.com>
2026-01-15 13:49:06 +0000

    5iem commit

--- src/main/java/sn/ufr/vacations/GestionVacationsApplication.java
+++ src/main/java/sn/ufr/vacations/GestionVacationsApplication.java
@@ -1,4 +1,14 @@
 package sn.ufr.vacations;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
 public class GestionVacationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionVacationsApplication.class, args);
+    }
 }
